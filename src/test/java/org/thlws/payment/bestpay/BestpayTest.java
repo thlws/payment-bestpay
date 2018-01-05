52011066b00e50ede8d476e8d5aa2800175052f2
hanley <hanley@thlws.com>
2018-01-05 22:09:39 +0800

    完善Junit Test

--- src/test/java/org/thlws/payment/bestpay/BestpayTest.java
+++ src/test/java/org/thlws/payment/bestpay/BestpayTest.java
@@ -1,5 +1,7 @@
 package org.thlws.payment.bestpay;
 
+import com.xiaoleilu.hutool.json.JSONUtil;
+import com.xiaoleilu.hutool.lang.Console;
 import com.xiaoleilu.hutool.util.RandomUtil;
 import org.junit.Test;
 import org.thlws.payment.bestpay.core.BestpayCore;
@@ -10,6 +12,12 @@ import org.thlws.payment.bestpay.entity.input.QueryOrderInput;
 import org.thlws.payment.bestpay.entity.output.OrderRefundOutput;
 import org.thlws.payment.bestpay.entity.output.OrderResultOutput;
 import org.thlws.payment.bestpay.entity.output.OrderReverseOutput;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by HanleyTang on 2018/1/5
@@ -29,6 +37,49 @@ public class BestpayTest {
 	/**商户号*/
 	private final  String merchantId = "02450107030126669";
 
+
+	/**
+	 * 支付-测试.
+	 */
+	@Test
+	public  void testPay() {
+
+		String barcode = "510094008028763507";
+		String orderAmt = "1";
+		String orderNo = RandomUtil.randomNumbers(16);
+
+		BarcodePayInput input = new BarcodePayInput();
+		input.setMerchantId(merchantId);
+		input.setBarcode(barcode);
+		input.setOrderNo(orderNo);
+		input.setOrderReqNo(orderNo);
+		input.setOrderDate("20171224121212");
+		input.setOrderAmt(orderAmt);
+		input.setProductAmt(orderAmt);
+		input.setGoodsName("测试商品");
+		input.setStoreId("00001025104487");
+
+		OrderResultOutput output = BestpayCore.barcode(input, key);
+		assertTrue(output.isPaySuccess());
+
+	}
+
+
+	/**
+	 * 查询交易-测试.
+	 */
+	@Test
+	public  void testQuery() {
+		QueryOrderInput input = new QueryOrderInput();
+		input.setMerchantId(merchantId);
+		input.setOrderNo("5241085967971143");
+		input.setOrderReqNo("5241085967971143");
+		input.setOrderDate("20171224121212");
+
+		OrderResultOutput output = BestpayCore.query(input, key);
+		assertNotNull(output.getResult());
+	}
+
 	/**
 	 * 撤销支付-测试.
 	 */
@@ -43,8 +94,9 @@ public class BestpayTest {
 		input.setRefundReqNo(RandomUtil.randomNumbers(16));
 		input.setRefundReqDate("20171224");
 		input.setTransAmt("1");
+
 		OrderReverseOutput output = BestpayCore.reverse(input, key);
-		System.out.println(output.toString());
+		assertNotNull(output.getResult());
 	}
 
 
@@ -63,53 +115,14 @@ public class BestpayTest {
 		input.setTransAmt("1");
 
 		OrderRefundOutput output = BestpayCore.refund(input, key);
-		System.out.println(output.toString());
+		assertNotNull(output.getResult());
 	}
 
 
-	/**
-	 * 查询交易-测试.
-	 */
-	@Test
-	public  void testQuery() {
-		QueryOrderInput input = new QueryOrderInput();
-		input.setMerchantId(merchantId);
-		input.setOrderNo("5241085967971143");
-		input.setOrderReqNo("5241085967971143");
-		input.setOrderDate("20171224121212");
-
-		OrderResultOutput output = BestpayCore.query(input, key);
-		System.out.println(output.toString());
-	}
 
 
-	/**
-	 * 支付-测试.
-	 */
-	@Test
-	public  void testPay() {
 
-		String barcode = "510094008028763507";
-		String orderAmt = "1";
 
-		String orderNo = RandomUtil.randomNumbers(16);
-		
-		BarcodePayInput input = new BarcodePayInput();
-		input.setMerchantId(merchantId);
-		input.setBarcode(barcode);
-		input.setOrderNo(orderNo);
-		input.setOrderReqNo(orderNo);
-		input.setOrderDate("20171224121212");
-		input.setOrderAmt(orderAmt);
-		input.setProductAmt(orderAmt);
-		input.setGoodsName("测试商品");
-		input.setStoreId("00001025104487");
 
-		OrderResultOutput output = BestpayCore.barcode(input, key);
-		System.out.println(output.toString());
-//		System.out.println(output.getResult().getOurTransNo());
-		System.out.println(output.isPaySuccess());
-
-	}
 
 }
