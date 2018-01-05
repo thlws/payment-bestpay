6194987211e16dfe23da32d4820bb2116aa5aff6
hanley <hanley@thlws.com>
2018-01-05 22:00:08 +0800

    抽离JSON格式化工具

--- src/main/java/org/thlws/payment/bestpay/utils/ThlwsJsonUtil.java
+++ src/main/java/org/thlws/payment/bestpay/utils/ThlwsJsonUtil.java
@@ -1,10 +1,33 @@
 package org.thlws.payment.bestpay.utils;
 
+import com.xiaoleilu.hutool.json.JSONUtil;
+
 /**
+ * JSON格式化工具
  * Created by HanleyTang on 2018/1/5
  *
- * @author Hanley[hanley@thlws.com]
+ * @author Hanley[hanley @thlws.com]
  * @version 1.0
  */
 public class ThlwsJsonUtil {
+
+    /**
+     * 格式化JSON.
+     *
+     * @param o 任意对象
+     * @return 格式友好的 JSON String
+     */
+    public static String format(Object o){
+        return JSONUtil.parse(o).toStringPretty();
+    }
+
+    /**
+     * 格式化JSON.
+     *
+     * @param jsonStr JSON格式字符串
+     * @return 格式友好的 JSON String
+     */
+    public static String format(String jsonStr){
+        return JSONUtil.formatJsonStr(jsonStr);
+    }
 }
